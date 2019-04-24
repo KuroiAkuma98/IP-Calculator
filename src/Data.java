6f5920e842a643249ff34fe627c68b7c7015024f
jarek stanczyk <jaretto66@interia.pl>
2019-04-24 17:53:38 +0200

    update Please enter the commit message for your changes. Lines starting

--- src/Data.java
+++ src/Data.java
@@ -1,39 +1,50 @@
 public class Data
 {
- String line;
- Data(String a)
- {
-     line = a;
- }
- boolean isCorrect()
- {
-     boolean flag = true;
+    String line;
+    Data(String a)
+    {
+        line = a;
+    }
+    boolean isCorrect()
+    {
+        boolean flag = true;
 
-     for(int i=0 ; i < this.line.length() ; i++ )
-     {
-         if(!(this.line.charAt(i) == '.' || this.line.charAt(i) == '/' || (this.line.charAt(i) >= '0' && this.line.charAt(i) <= '9')))
-         {
-             flag =  false;
-             break;
-         }
-     }
+        for(int i=0 ; i < this.line.length() ; i++ )
+        {
+            if(!(this.line.charAt(i) == '.' || this.line.charAt(i) == '/' || (this.line.charAt(i) >= '0' && this.line.charAt(i) <= '9')))
+            {
+                flag =  false;
+                break;
 
-     int temp = this.line.indexOf(".");
-     int tmp;
-     while(temp != this.line.lastIndexOf("."))
-     {
-         if(temp+1 == this.line.indexOf(".",temp))
-         {
-             flag = false;
-             break;
-         }
-         else
-             {
-             tmp = temp;
-             temp = this.line.indexOf(".", tmp);
-         }
-     }
+            }
+            if(this.line.charAt(i) == '.' && this.line.charAt(i+1) == '.' )
+            {
+                flag = false;
+                break;
+            }
+            //System.out.println(i);
+        }
 
-     return flag;
- }
+        String ip="";
+        int a,b,c,d,mask;
+
+        String[] splitedArray1 = null;
+        splitedArray1 = this.line.split("/");
+        ip = splitedArray1[0];
+        mask = Integer.parseInt(splitedArray1[1]);
+        System.out.println("Twoje IP: " + ip + " twoja maska: " + mask);
+        String[] splitedArray2;
+        splitedArray2 = ip.split("\\.");
+        a = Integer.parseInt(splitedArray2[0]);
+        b = Integer.parseInt(splitedArray2[1]);
+        c = Integer.parseInt(splitedArray2[2]);
+        d = Integer.parseInt(splitedArray2[3]);
+
+        System.out.println(a + " " + b + " " + c + " " + d );
+        if(!((mask >=0 && mask <=32) && (a > 0 && a < 256) && (b >= 0 && b < 256) && (c >= 0 && c < 256) && (d >= 0 && d < 256)))
+        {
+            flag = false;
+        }
+        return flag;
+    }
 }
--- src/IpCalculator.java
+++ src/IpCalculator.java
@@ -1,35 +1,51 @@
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Scanner;
+
 
 public class IpCalculator
 {
-    public static void main(String[] args)
+    public static String ipToBin(String ip)
+    {
+
+    }
+    public static void main(String[] args) throws UnknownHostException, SocketException
     {
         System.out.println("Wpisz adres komputera:");
         Scanner scan = new Scanner(System.in);
         String data = scan.nextLine();
+        //String data = args[0];
+        //System.out.println(data);
 
         // Ewentualne pobranie wlasnego adresu w przypadku gdy nie podano adresu
         if(data.isEmpty())
         {
             System.out.println("nie podano adresu, adres zostanie pobrany z uzywanego urzadzeina");
-            InetAddress adres= InetAddress.getLocalHost();
-            adres.getAddress();
-        }
+            InetAddress ipAdres = InetAddress.getLocalHost();
+            System.out.println("twoj adres ip: " + ipAdres.getHostAddress());
+            NetworkInterface Mask = NetworkInterface.getByInetAddress(ipAdres);
+            System.out.println("twoja maska ip: " + Mask.getInterfaceAddresses().get(0).getNetworkPrefixLength());
+            String ip_adres = ipAdres.getHostAddress().toString();
+            int mask = Mask.getInterfaceAddresses().get(0).getNetworkPrefixLength();
 
-
-
-        Data your_data = new Data(data);
-        if(your_data.isCorrect())
-        {
-            //String ip="",a="",b="",c="",d="",mask="";
-            //String[] splitedArray = null;
-            System.out.println("Brawo! Wpisales/as dobre dane");
         }
         else
         {
-            System.out.println("Caly misterny plan w pizdu :<");
+            Data your_data = new Data(data);
+            if(your_data.isCorrect())
+            {
+                System.out.println("Brawo! Wpisales/as dobre dane");
+            }
+            else
+            {
+                System.out.println("Niepoprawne IP!");
+            }
         }
+
+
+
 
 
 
